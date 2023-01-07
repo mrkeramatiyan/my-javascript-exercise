d1d348f259e1223c71dbd60e5b219daee6e5b151
mrkeramatiyan <70322925+mrkeramatiyan@users.noreply.github.com>
2023-01-07 16:06:19 +0330

    some change & refactor
    - Add toaster for alert and confrim
    - I used your suggestions in code like Array.some and default operator
    - I refactored my todolist for duplicat task

--- todo-list/script.js
+++ todo-list/script.js
@@ -1,23 +1,26 @@
 class Task {
-    constructor(label) {
+    constructor(label, id) {
         this.label = label;
         this.isCompeleted = false;
+        this.taskId = id;
     }
 }
 
 class TodoList {
     
     constructor(elementInitApp, preset = {}) {
-        this.titleOfApp = preset.title ? preset.title : 'Todo List App';
-        this.idOfApp = preset.id ? preset.id : '';
+        this.titleOfApp = preset.title || 'Todo List App';
+        this.idOfApp = preset.id || '';
         this.elementInitApp = elementInitApp;
         this.state = [];
+        this.arrayOfLiElementsToaster = [];
+        this.isAlertBoxActive = true;
         this.createStructureApp();
         this.checkDataForState();
     }
 
     checkDataForState() {
-        const getDataFromlocalStorage = localStorage.getItem(this.elementInitApp.getAttribute('id'));
+        const getDataFromlocalStorage = localStorage.getItem(this.idOfApp || this.elementInitApp.getAttribute('id'));
         if(getDataFromlocalStorage) {
             this.state = JSON.parse(getDataFromlocalStorage);
             this.renderTodoTasks();
@@ -31,7 +34,7 @@ class TodoList {
         const element =  document.createElement(elementName);
         if(addClass) element.classList.add(addClass);
         if(value) element.innerText = value;
-        if(id) element.setAttribute('id', id + '');
+        if(id) element.setAttribute('id', id);
         return element;
     }
 
@@ -42,44 +45,45 @@ class TodoList {
         // create Title
         const h1 = this.createElement('h1');
         h1.innerText = this.titleOfApp;
-        wrapperElementApp.appendChild(h1);
+        wrapperElementApp.append(h1);
 
         const wrapperInsertTask = this.createElement('section','', 'wrapper-insert');
         const addTaskBtn = this.createElement('button', 'Add Task', 'add-task-btn');
         const inputElement = this.createElement('input', '', 'input-text-task');
         inputElement.setAttribute('type','text');
         inputElement.setAttribute('placeholder','Enter New Task');
-        wrapperInsertTask.appendChild(addTaskBtn);
-        wrapperInsertTask.appendChild(inputElement);
-        wrapperElementApp.appendChild(wrapperInsertTask);
+        wrapperInsertTask.append(addTaskBtn);
+        wrapperInsertTask.append(inputElement);
+        wrapperElementApp.append(wrapperInsertTask);
 
         const wrapperListOfTasks = this.createElement('ul', '', 'wrapper-list-taks');
-        wrapperElementApp.appendChild(wrapperListOfTasks);
+        wrapperElementApp.append(wrapperListOfTasks);
 
-        // get context this for use inside functions
+        // get context this for use inside function
         const mainContext = this;
         // function of event
         function handlerAddTaskBtn(event) {
             let value = '';
-            // console.log(this.tagName)
             if(this.tagName == 'INPUT') {
                 value = this.value.trim();
             } else if(this.tagName == 'BUTTON') {
-                value = this.closest('section').querySelector('.input-text-task').value;         
+                value = this.closest('section').querySelector('.input-text-task').value.trim();     
             }
             
-            const result = Boolean(mainContext.state.find(taskObj => taskObj.label == value));
-            if(result) {
-                alert('Your task is already have!!!')
-            }
-            if(value != '' && value != ' ' && !result) {
-                const newTask = new Task(value);
+            if(value.length > 0 ) {
+                const uniqueIdTask = mainContext.genarateUID(6);
+                const newTask = new Task(value, uniqueIdTask);
                 mainContext.state = [
                     ...mainContext.state,
                     newTask
                 ];
                 event.target.value = '';
                 mainContext.renderTodoTasks();
+            } else {
+                if(mainContext.isAlertBoxActive) {
+                    mainContext.isAlertBoxActive = false;
+                    mainContext.runToaster('You should enter something for add task!');
+                }
             }
         };
 
@@ -87,19 +91,19 @@ class TodoList {
         addTaskBtn.addEventListener('click', handlerAddTaskBtn);
         inputElement.addEventListener('change', handlerAddTaskBtn);
 
-        this.elementInitApp.appendChild(wrapperElementApp);
+        this.elementInitApp.append(wrapperElementApp);
     }
 
     renderTodoTasks() {
         const fragment = document.createDocumentFragment();
         const wrapperListOfTasks = this.elementInitApp.querySelector('.wrapper-todo-app .wrapper-list-taks');
         wrapperListOfTasks.innerHTML = '';
-
-        this.state.forEach((taskObj, index) => {
+        
+        this.state.forEach((taskObj) => {
             const guid = this.genarateUID(5);
 
             const li = this.createElement('li');
-            li.setAttribute('id', `${index}`);
+            li.setAttribute('id', taskObj.taskId);
 
             const label = this.createElement('label');
             label.setAttribute('for', guid);
@@ -111,19 +115,19 @@ class TodoList {
             if(taskObj.isCompeleted) {
                 checkbox.checked = true;
             }
-            
+
             const editBtn = this.createElement('button', 'Edit','edit-btn');
             const deleteBtn = this.createElement('button', 'Delete', 'delete-btn');
 
             label.prepend(checkbox);
-            li.appendChild(label);
-            li.appendChild(editBtn);
-            li.appendChild(deleteBtn);
+            li.append(label);
+            li.append(editBtn);
+            li.append(deleteBtn);
 
             // functions of events
             const handlerEditBtn = (event) => {
                 const li = event.target.closest('li');
-                const id = parseInt(event.target.closest('li').getAttribute('id'), 10);
+                const id = event.target.closest('li').getAttribute('id');
                 const label = li.querySelector('label');
                 
                 const input = this.createElement('input');
@@ -135,7 +139,8 @@ class TodoList {
                 
                 // functions
                 const handelerChange = (event) => {
-                    this.state[id].label = event.target.value;
+                    const objOfTask = this.state.filter(objTask => objTask.taskId == id);
+                    objOfTask[0].label = event.target.value;
                     this.renderTodoTasks();
 
                     input.removeEventListener('change', handelerChange);
@@ -156,18 +161,23 @@ class TodoList {
             };
 
             const handlerDeleteBtn = (event) => {
-                const id = parseInt(event.target.closest('li').getAttribute('id'), 10);
-                const value = event.target.closest('li').querySelector('label').innerText;
-                if(confirm(`Are you sure for delete task? => ${value}`)) {
-                    this.state = this.state.filter(taskObj => taskObj.label != value);
-                    this.renderTodoTasks();
+                const id = event.target.closest('li').getAttribute('id');
+                const objOfTask = this.state.filter(objTask => objTask.taskId == id);
+                const value = objOfTask[0].label;
+                this.runToaster(`Are you sure for delete task? => ${value}`, true, callBackForResult, id);
+
+                const mainContext = this;
+                function callBackForResult() {
+                    mainContext.state = mainContext.state.filter(taskObj => taskObj.taskId != id);
+                    mainContext.renderTodoTasks();
                 }
-                deleteBtn.removeEventListener('click', handlerDeleteBtn);
             };
 
             const handlerChecked = (event) => {
-                const id = parseInt(event.target.closest('li').getAttribute('id'), 10);
-                this.state[id].isCompeleted = !this.state[id].isCompeleted;
+                const id = event.target.closest('li').getAttribute('id');
+                const objOfTask = this.state.filter(objTask => objTask.taskId == id);
+                objOfTask[0].isCompeleted = !objOfTask[0].isCompeleted;
+
                 this.renderTodoTasks();
                 label.removeEventListener('click', handlerChecked);
             };
@@ -177,18 +187,18 @@ class TodoList {
             deleteBtn.addEventListener('click', handlerDeleteBtn);
             label.addEventListener('click', handlerChecked);
 
-            fragment.appendChild(li);
+            fragment.append(li);
         });
-        
+               
         // Add Taks to DOM
-        wrapperListOfTasks.appendChild(fragment);
+        wrapperListOfTasks.append(fragment);
 
         // After any call renderTodoTasks method save to localStorage
         this.saveToLocalStorage();
     }
 
     saveToLocalStorage() {
-        const nameDataFromId = this.idOfApp ? this.idOfApp : this.elementInitApp.getAttribute('id');
+        const nameDataFromId = this.idOfApp || this.elementInitApp.getAttribute('id');
         const data = JSON.stringify(this.state);
         localStorage.setItem(nameDataFromId, data);
     }
@@ -213,6 +223,79 @@ class TodoList {
         min = Math.ceil(min);
         max = Math.floor(max);
         return Math.floor(Math.random() * (max - min) + min);
+    }
+
+    runToaster(_text, _confirm = false, _callBackFn, _targetId) {
+        const mainContext = this;
+        const existToaster = [...document.querySelector('body').children].some(item => item.className == 'toaster');
+        
+        if(!existToaster) {
+            const bodyElement = document.body;
+            const createToasterElement = this.createElement('ul', '', 'toaster', this.guidForToaseter);
+            bodyElement.append(createToasterElement);
+        }
+
+        const toasterElement = document.querySelector('.toaster');
+        
+        // Check Box is confirm or simple
+        if(_confirm) {
+            const wrapperButtons = this.createElement('div', '', 'wrapper-btn');
+            wrapperButtons.hidden = false;
+            const toasterYesButton = this.createElement('button', 'Yes', 'yes-btn', '');
+            const toasterCancelButton = this.createElement('button', 'Cancel', 'cancel-btn', '');
+            wrapperButtons.append(toasterYesButton, toasterCancelButton);            
+            const existToaster = this.arrayOfLiElementsToaster.some(itemObj => itemObj.targetId == _targetId);
+            let liToaster;
+            if(!existToaster) {
+                liToaster = createLi(true, wrapperButtons);
+            }
+
+            // add Event Listener
+            toasterYesButton.addEventListener('click', function handlerYesBtn() {
+                _callBackFn();
+ 
+                mainContext.arrayOfLiElementsToaster = mainContext.arrayOfLiElementsToaster.filter(toasterObj => toasterObj.id != liToaster.id);
+                liToaster.el.remove();
+                toasterYesButton.removeEventListener('click', handlerYesBtn);
+            });
+            toasterCancelButton.addEventListener('click', function handlerCancelBtn() {
+                liToaster.el.remove();
+                toasterCancelButton.removeEventListener('click', handlerCancelBtn);
+            });            
+            
+            // Add to DOM
+            if (liToaster) {
+                toasterElement.append(liToaster.el);
+            }
+        } else {
+            const li = createLi();
+            toasterElement.append(li.el);
+            
+            setTimeout(() => {
+                li.el.remove();
+
+                // for prevent repeat alert box
+                this.isAlertBoxActive = true;
+            }, 5000);
+        }    
+                
+        // helper function
+        function createLi(haveButton = false, element) {
+            const id = mainContext.genarateUID(5);
+            const li = mainContext.createElement('li');
+            const textToasteser = mainContext.createElement('span', _text,'text-toaster');
+            li.append(textToasteser);
+            if(haveButton) {
+                li.append(element);
+            }
+            const obj = {
+                id,
+                el: li,
+                targetId: _targetId
+            };
+            mainContext.arrayOfLiElementsToaster.push(obj);
+            return obj;
+        }
     }
 }
 
